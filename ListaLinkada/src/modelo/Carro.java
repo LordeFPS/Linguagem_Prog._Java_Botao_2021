2ea7e7bead05f26580034199c85f9bdcb1c853c3
lordefps <lodizinho_mice@hotmail.com>
2021-02-14 17:16:08 -0300

    Getters & Setters

--- ListaLinkada/src/modelo/Carro.java
+++ ListaLinkada/src/modelo/Carro.java
@@ -10,11 +10,67 @@ package modelo;
  * @author 69146
  */
 public class Carro {
-    String placa;
-    String modelo;
-    String marca;
-    int anoFabricacao;
-    String cor;
+    private String placa;
+    private String modelo;
+    private String marca;
+    private int anoFabricacao;
+    private String cor;
+
+    public Carro() {
+    }
+
+    public Carro(String placa, String modelo, String marca, int anoFabricacao, String cor) {
+        this.placa = placa;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.anoFabricacao = anoFabricacao;
+        this.cor = cor;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" + "placa=" + placa + ", modelo=" + modelo + ", marca=" + marca + ", anoFabricacao=" + anoFabricacao + ", cor=" + cor + '}';
+    }
     
     
 }
