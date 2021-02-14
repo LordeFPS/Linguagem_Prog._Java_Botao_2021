2059525201633174b4b7ca432bd6324eec876de8
lordefps <lodizinho_mice@hotmail.com>
2021-02-14 15:14:05 -0300

    Manipulando Lista com métodos

--- Linguagem_Prog_Java/src/Aula1/Aula1.java
+++ Linguagem_Prog_Java/src/Aula1/Aula1.java
@@ -7,23 +7,95 @@ package Aula1;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
  * @author 69146
  */
 public class Aula1 {
-
-    /**
-     * @param args the command line arguments
-     */
+    static Scanner leitor = new Scanner(System.in);
+    static List<String> lista = new ArrayList<>();
+            
     public static void main(String[] args) {
+        //manipulandoListas();
+        usuarioLista();
+    }
+    
+    public static void manipulandoListas() {
         List<String> carros = new ArrayList<>();
         carros.add("Fusca");
         carros.add("Kombi");
         carros.add("Chevette");
+        carros.add("Brasilia");
         
-        System.out.println(carros.size());
+        for (int i = 0; i < carros.size(); i++){
+            System.out.println(carros.get(i));
+        }
+        
+        System.out.println("");
+            
+        carros.add(0,"Troller");
+        carros.add("TL");
+        for (String carro: carros ){
+            System.out.println(carro);
+        }
+        
+        //System.out.println(carros.indexOf("Troller"));
+        
+        if (carros.indexOf("TL") != -1){
+            System.out.println("TL - Já existe");
+        }else {
+            System.out.println("TL - Não existe");
+        }
     }
     
+    public static void usuarioLista() {
+         /*
+        1 - Criar um método para inserir dados na lista;
+            O usuário vai digitar os dados;
+            Não permitir registros duplicados na lista;
+        2 - Criar um método para listar os registros inseridos;
+        */
+        boolean continuar = true;
+        while (continuar){
+            System.out.println("Digite uma marca");
+            String marca = leitor.next();
+            inserirLista(marca);
+            
+            System.out.println("Deseja continuar? (S/N) ");
+            String opcao = leitor.next();
+            
+            continuar = opcao.toUpperCase().equals("S");
+        }
+        
+        listarLista();
+    }
+    public static void inserirLista(String str) {
+        /*
+        1 - Criar um método para inserir dados na lista;
+            O usuário vai digitar os dados;
+            Não permitir registros duplicados na lista;
+        2 - Criar um método para listar os registros inseridos;
+        */
+        if (lista.contains(str)){//verificar se existe algo dentro da lista
+        //if (lista.indexOf(str) >= 0){
+            System.out.println("Este valor ja existe");  
+        }else {
+            lista.add(str);
+        }
+    }
+    
+    public static void listarLista() {
+        /*
+        1 - Criar um método para inserir dados na lista;
+            O usuário vai digitar os dados;
+            Não permitir registros duplicados na lista;
+        2 - Criar um método para listar os registros inseridos;
+        */
+        System.out.println("Lista ↓");
+        for (String listar : lista){
+            System.out.println(listar);
+        }
+    }
 }
