3d58f4ea46b5a5da24736057b925bcb69c5f8e38
lordefps <lodizinho_mice@hotmail.com>
2021-02-14 14:16:39 -0300

    Aula 1 Programando em Java

---
+++ Linguagem_Prog_Java/src/Aula1/Aula1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author 69146
+ */
+public class Aula1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        List<String> carros = new ArrayList<>();
+        carros.add("Fusca");
+        carros.add("Kombi");
+        carros.add("Chevette");
+        
+        System.out.println(carros.size());
+    }
+    
+}
