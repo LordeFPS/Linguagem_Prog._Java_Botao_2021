a6ea5b742b0b88169079f0416855961c0df16520
lordefps <lodizinho_mice@hotmail.com>
2021-02-21 18:33:03 -0300

    Criando a entidade carro

---
+++ CadastroSwing_Aula2/src/cadastroswing/entidades/Carro.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastroswing.entidades;
+
+/**
+ *
+ * @author 69146
+ */
+public class Carro {
+    private String placa;
+    private String marca;
+    private String modelo;
+    private int anoFabricacao;
+
+    public Carro() {
+    }
+
+    public Carro(String placa, String marca, String modelo, int anoFabricacao) {
+        this.placa = placa;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public int getAnoFabricacao() {
+        return anoFabricacao;
+    }
+
+    public void setAnoFabricacao(int anoFabricacao) {
+        this.anoFabricacao = anoFabricacao;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" + "placa=" + placa + ", marca=" + marca + ", modelo=" + modelo + ", anoFabricacao=" + anoFabricacao + '}';
+    }
+    
+    
+}
