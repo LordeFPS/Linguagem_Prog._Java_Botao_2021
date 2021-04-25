ea64ad68a7bd7735a465a30970189d5d65081e82
lordefps <lodizinho_mice@hotmail.com>
2021-04-25 17:19:51 -0300

    Aula 7 Conexao com o Banco de Dados

---
+++ Cadastro_Vendas/src/utils/ConexaoDB.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author 69146
+ */
+public class ConexaoDB {
+    public static Connection getConexao(){
+        Connection con = null;
+        String url = "jdbc:postgresql://localhost:5432/vendas";
+        String user = "postgres";
+        String password = "admin";
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException | SQLException ex) {
+            System.out.println("Erro ao conectar com o banco");
+        }
+        return con;
+    }
+}
