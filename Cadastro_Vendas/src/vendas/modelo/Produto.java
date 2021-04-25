d82d7318acac740218235b95d729132a81752c7f
lordefps <lodizinho_mice@hotmail.com>
2021-04-25 18:42:06 -0300

    Aula 7 Cadastro Produto

---
+++ Cadastro_Vendas/src/vendas/modelo/Produto.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vendas.modelo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import utils.ConexaoDB;
+
+/**
+ *
+ * @author 69146
+ */
+public class Produto {
+    private int idproduto;
+    private String descricao;
+    private float preco;
+    private float estoque;
+
+    public Produto(String descricao, float preco, float estoque) {
+        this.descricao = descricao;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public Produto(int idproduto, String descricao, float preco, float estoque) {
+        this.idproduto = idproduto;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    public int getIdproduto() {
+        return idproduto;
+    }
+
+    public void setIdproduto(int idproduto) {
+        this.idproduto = idproduto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public float getPreco() {
+        return preco;
+    }
+
+    public void setPreco(float preco) {
+        this.preco = preco;
+    }
+
+    public float getEstoque() {
+        return estoque;
+    }
+
+    public void setEstoque(float estoque) {
+        this.estoque = estoque;
+    }
+    
+    public boolean salvar() {
+        try {
+            //conectar ao banco
+            Connection  con = ConexaoDB.getConexao();
+            //montar sql
+            String sql = " insert into produto (descricao, preco, estoque)" +
+                         " values (?,?,?);";
+            //envia sql para o banco de dados
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, this.descricao);
+            stm.setFloat(2, this.preco);
+            stm.setFloat(3, this.estoque);
+            stm.execute();
+        } catch (SQLException ex){
+            throw new RuntimeException("Erro cadastro produto: " + ex.getMessage());
+            //System.out.println("Erro: " + ex.getMessage());
+            //return false;
+        }
+        return true;
+    }
+}
