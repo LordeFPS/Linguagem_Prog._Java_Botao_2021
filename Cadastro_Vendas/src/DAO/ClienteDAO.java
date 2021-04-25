51b090f1180f0bed7410b6284327ad0fa7459d7a
lordefps <lodizinho_mice@hotmail.com>
2021-04-25 18:41:07 -0300

    Aula 7 Criando DAO

---
+++ Cadastro_Vendas/src/DAO/ClienteDAO.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import utils.ConexaoDB;
+import vendas.modelo.Cliente;
+
+/**
+ *
+ * @author 69146
+ */
+public class ClienteDAO{
+    public static boolean salvar(Cliente clt) {
+        try {
+            //conectar ao banco
+            Connection  con = ConexaoDB.getConexao();
+            //montar sql
+            String sql = " insert into cliente (nome, fone, email)" +
+                         " values (?,?,?);";
+            //envia sql para o banco de dados
+            PreparedStatement stm = con.prepareStatement(sql);
+            stm.setString(1, clt.getNome());
+            stm.setString(2, clt.getFone());
+            stm.setString(3, clt.getEmail());
+            stm.execute();
+        } catch (SQLException ex){
+            throw new RuntimeException("Erro cadastro cliente: " + ex.getMessage());
+            //System.out.println("Erro: " + ex.getMessage());
+            //return false;
+        }
+        return true;
+    }
+    
+}
