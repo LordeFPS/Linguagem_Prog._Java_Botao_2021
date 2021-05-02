9a346c4a792006808184e0e111ba4614a65e28a2
lordefps <lodizinho_mice@hotmail.com>
2021-05-02 17:39:35 -0300

    Aula 8 interface DAO

---
+++ Cadastro_Vendas/src/DAO/interfaces/DaoInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO.interfaces;
+
+import java.sql.ResultSet;
+/**
+ *
+ * @author 69146
+ */
+public interface DaoInterface {
+    public boolean salvar();
+    public boolean editar();
+    public ResultSet getAll();
+    public Object getById(int id);
+}
