82bf42b6f42f978b0d82de0688e7697df98cdcd1
lordefps <lodizinho_mice@hotmail.com>
2021-05-09 18:09:27 -0300

    Aula 9 Inserindo dados na tabela com JPA

---
+++ JPA/src/jpa/JPA.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpa;
+
+import java.sql.Date;
+
+/**
+ *
+ * @author 69146
+ */
+public class JPA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*Categoria cat = new Categoria("Cereais");
+        DAO.persist(cat);
+        
+        Categoria cat2 = new Categoria("Carne");
+        DAO.persist(cat2);
+        
+        Categoria cat3 = new Categoria("Verduras");
+        DAO.persist(cat3);
+        
+        Produto pd = new Produto("Amendoas", 10, cat);
+        DAO.persist(pd);
+        
+        Produto pd2 = new Produto("Alcatara", 50, cat2);
+        DAO.persist(pd2);
+        
+        Produto pd3 = new Produto("Tomate", 20, cat3);
+        DAO.persist(pd3);
+        
+        Categoria catRetorno = DAO.getCategoria(3);
+        catRetorno.setDescricao("Carnes e derivados");
+        
+        DAO.update(catRetorno);
+        catRetorno = DAO.getCategoria(3);
+        System.out.println(catRetorno.toString());
+        
+        Venda venda = new Venda(Date.valueOf("2021-05-09"));
+        DAO.persist(venda);
+        */
+        /*
+        Itens item2 = new Itens(2,20 , DAO.getProduto(4L) , DAO.getVenda(7L));
+        DAO.persist(item2);
+        
+        Itens item = new Itens(2,100 , DAO.getProduto(5L), DAO.getVenda(7L));
+        DAO.persist(item);
+        
+        Itens item3 = new Itens(2,30 , DAO.getProduto(6L), DAO.getVenda(7L));
+        DAO.persist(item3);
+        */
+        
+        
+        //DAO.delete(1);
+        
+        
+    }
+    
+    
+}
